fa8421b17ac05fa3cd58d3af250e2be7b12ec279
Hassan Munir <91518406+HassanMunir@users.noreply.github.com>
2023-10-30 11:32:08 +0500

    terr-version-upd

---
+++ 01-versions.tf
@@ -0,0 +1,29 @@
+# Creating Terraform Block
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Creating Provider Block
+provider "aws" {
+  region  = var.aws_region
+  profile = "default"
+}
+
+# Creating Random Pet Resource
+resource "random_pet" "this" {
+  length = 2
+}
